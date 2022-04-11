ccc58239dc2cc7a529676c882b28fe59e33afdb4
Ankur-ssj <ankursinha136@gmail.com>
2022-04-11 17:51:31 +0530

    made-models-update

---
+++ Stock-Prediction-app-master/Stock-Prediction-app-master/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/Home" element={<Home/>}/>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Stock-Prediction-app-master/Stock-Prediction-app-master/src/Pages/Home.js
@@ -0,0 +1,48 @@
+import React, { useRef, useEffect } from 'react'
+import '../Scss/Home.css'
+import { GraphModel } from '../Models/ModelsExport'
+import gsap from 'gsap'
+
+const Home = () => {
+
+  let homeOne = useRef(null);
+  let homeTwo = useRef(null);
+
+  const staggerText = (node) => {
+    gsap.from(node, {
+        duration:0.8,
+        y: 200,
+        delay: 1.5,
+        ease: "circ.out",
+        opacity: 0,
+        stagger: {
+            amount: 0.3
+        },
+    });
+  } 
+
+  const fadeIn = (node) =>{
+    gsap.from(node,{
+      duration: 1,
+      delay: 1,
+      ease: "circ.out",
+      opacity:0
+    });
+  }
+
+  useEffect(() => {
+    staggerText(homeOne);
+    fadeIn(homeTwo)
+})
+  
+  return (
+    <div className='home_container'>
+        <h1 ref={el => (homeOne = el)} className='home_title'>MAKING YOUR TRADING SMOOTHER.</h1>
+        <div ref={el => (homeTwo = el)} className='home_canvas'>
+            <GraphModel/>
+        </div>
+    </div>
+  )
+}
+
+export default Home
